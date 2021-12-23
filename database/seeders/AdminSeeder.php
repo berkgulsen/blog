e7d2dca3be3b19a12199896ae58abfe877c726a3
berkgulsen <83736291+berkgulsen@users.noreply.github.com>
2021-12-23 23:58:47 +0300

    admin authentication added

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'name'=>'Berk Gülşen',
+            'email'=>'berkglsn48@gmail.com',
+            'password'=>bcrypt('password'),
+
+        ]);
+    }
+}
--- resources/views/back/auth/login.blade.php
+++ resources/views/back/auth/login.blade.php
@@ -41,27 +41,26 @@
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Hoşgeldiniz</h1>
                                 </div>
-                                <form class="user">
+                                @if($errors->any())
+                                    <div class="alert alert-danger">
+                                        {{$errors->first()}}
+                                    </div>
+                                @endif
+                                <form method="POST" action="{{route('admin.login.post')}}" class="user">
+                                    @csrf
                                     <div class="form-group">
                                         <input type="email" class="form-control form-control-user"
                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                               placeholder="Email Adresi">
+                                               placeholder="Email Adresi" name="email">
                                     </div>
                                     <div class="form-group">
                                         <input type="password" class="form-control form-control-user"
-                                               id="exampleInputPassword" placeholder="Şifre">
+                                               id="exampleInputPassword" placeholder="Şifre" name="password">
                                     </div>
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox small">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck">
-                                            <label class="custom-control-label" for="customCheck">
-                                                Beni Hatırla
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <a href="index.html" class="btn btn-primary btn-user btn-block">
+
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
                                         Giriş Yap
-                                    </a>
+                                    </button>
                                 </form>
                             </div>
                         </div>
