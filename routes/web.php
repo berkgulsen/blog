f0df22ed55ebd89a2edef103161ba926a772c8a6
berkgulsen <83736291+berkgulsen@users.noreply.github.com>
2021-12-23 21:30:51 +0300

    admin panel added login page edited

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,24 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Backend Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+*/
+
+Route::get('/admin/panel','App\Http\Controllers\Back\Dashboard@index')->name('admin.dashboard');
+Route::get('/admin/giris','App\Http\Controllers\Back\Auth@login')->name('admin.login');
+
+
+/*
+|--------------------------------------------------------------------------
+| Front Routes
+|--------------------------------------------------------------------------
 */
 
 Route::get('/','App\Http\Controllers\Front\Homepage@index')->name('homepage');
 Route::get('/iletisim','App\Http\Controllers\Front\Homepage@contact')->name('contact');
 Route::post('/iletisim','App\Http\Controllers\Front\Homepage@contactpost')->name('contact.post');
-
 Route::get('/{sayfa}','App\Http\Controllers\Front\Homepage@page')->name('page');
 Route::get('/kategori/{category}','App\Http\Controllers\Front\Homepage@category')->name('category');
-
 Route::get('/{category}/{slug}','App\Http\Controllers\Front\Homepage@single')->name('single');
 
